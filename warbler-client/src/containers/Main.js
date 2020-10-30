dd7074fdd53c80313d0afaa17db2120d6e6a22f6
Rich Wright <rchwrght@gmail.com>
2020-10-30 14:11:14 +0100

    authentication issue when posting new message

--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -5,6 +5,8 @@ import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
 import { authUser } from "../store/actions/auth";
 import { removeError } from "../store/actions/errors";
+import withAuth from "../hocs/withAuth";
+import MessageForm from "../containers/MessageForm";
 
 const Main = props => {
 	const { authUser, errors, removeError, currentUser } = props;
@@ -36,6 +38,9 @@ const Main = props => {
 							{...props} />
 					)
 				}} />
+				<Route path="/users/:id/messages/new" 
+					component={withAuth(MessageForm)} 
+				/>
 			</Switch>
 		</div>
 	)
--- warbler-server/index.js
+++ warbler-server/index.js
@@ -24,7 +24,7 @@ app.use("/api/users/:id/messages",
 	messagesRoutes
 );
 
-app.get("api/messages", loginRequired, async function(req,res,next){
+app.get("api/messages", loginRequired, async function(req,res,next) {
  try{
  	let messages = await db.Message.find()
  		.sort({ createdAt:"desc" })
